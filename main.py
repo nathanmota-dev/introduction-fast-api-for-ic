02324cdde6cfc482f89d44471421413e74c63351
Nathan <nathansmota@gmail.com>
2024-03-10 23:14:06 -0300

    :sparkles: feat: adding status, delete and put

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
-from fastapi import HTTPException
+from fastapi import FastAPI, HTTPException, status
+
 
 app = FastAPI()
 
@@ -25,7 +26,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/create")
+@app.post("/create", status_code=status.HTTP_201_CREATED)
 def create_values(new_message: Message):
     try:
         dict_message = new_message.model_dump()
@@ -46,4 +47,41 @@ def find_message_for_id(message, id):
 @app.get("/messages/{id}")
 def get_msg(id: int):
     found_message = find_message_for_id(my_messages, int(id))
+    if not found_message:
+        raise HTTPException(status_code=404, detail="Mensagem não encontrada")
     return {"Mensagem Encontrada": found_message}
+
+
+def find_index_by_id(messages, id):
+    for i, message in enumerate(messages):
+        if message["id"] == id:
+            return i
+    return None
+
+
+@app.delete("/messages/{id}")
+def delete_msg(id: int):
+    index = find_index_by_id(my_messages, id)
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Mensagem com o id {id} não encontrada",
+        )
+    else:
+        my_messages.pop(index)
+    return {"Mensagem Deletada": id}
+
+
+@app.put("/mensages/{id}")
+def update_msg(id: int, update_mensagem=Message):
+    index = find_index_by_id(my_messages, id)
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Mensagem com o id {id} não encontrada",
+        )
+    else:
+        dict_message = update_mensagem.model_dump()
+        dict_message["id"] = id
+        my_messages[index] = update_mensagem.dict_message
+    return {"Mensagem Atualizada": my_messages[index]}
